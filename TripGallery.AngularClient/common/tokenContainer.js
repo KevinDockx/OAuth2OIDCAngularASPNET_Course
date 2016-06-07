f89aaddf12a94b6ccc594777b03b3aa91897998f
KevinDockx <kevin.dockx@gmail.com>
2016-06-07 18:36:21 +0200

    Add tokenContainer starter file

---
+++ TripGallery.AngularClient/common/tokenContainer.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("tokenContainer",
+                  [tokenContainer])
+
+    function tokenContainer() {
+   
+        var container = {
+            token: ""
+        };
+         
+        var setToken = function (token) {
+            container.token = token;
+        };
+
+        var getToken = function () {
+            if (container.token === "") {
+                if (localStorage.getItem("access_token") === null) {
+ 
+                }
+                else {
+                    // set the token in localstorage
+                    setToken(localStorage["access_token"]);
+                }
+            }
+            return container;
+        };
+
+        // return value to call when calling the API 
+        return {
+            getToken: getToken
+        };
+        
+
+    };
+
+})();
